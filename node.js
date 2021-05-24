6f49755f114694714afae7a473c1a05123c239f9
sri2k21 <83586622+sri2k21@users.noreply.github.com>
2021-05-24 13:27:23 +0530

    Update node.js
    changes done

--- node.js
+++ node.js
@@ -1,7 +1,7 @@
 var http =require('http');
-
   http.createServer(function (req, res)
-   {
+  {
+   res.writeHead(200,{'Content-Type':'text/plain'));
    res.end('Hello Worl!');
    }
    ).listen(5000);
